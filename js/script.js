f1c2eba95a9e141a2edc4d95560d721dcbdfce74
Irina14 <ira1786@bk.ru>
2018-08-08 08:45:53 +0700

    Сделано модальное окно и поля

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ var control_1 = document.querySelector('.control-1');
 var control_2 = document.querySelector('.control-2');
 var control_3 = document.querySelector('.control-3');
 var body = document.querySelector('body');
+var feedback = document.querySelector('.map-contacts__button');
+var close = document.querySelector('.modal__close');
+var modal = document.querySelector('.modal');
+var overlay = document.querySelector('.modal__overlay');
 
 
 
@@ -21,4 +25,20 @@ control_3.addEventListener('click', function (evt) {
   body.classList.add('background-3');
   body.classList.remove('background-2');
   body.classList.remove('background-1');
+});
+
+feedback.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modal.classList.add('modal-open');
+  modal.classList.remove('modal-close');
+  overlay.classList.add('modal__overlay--open');
+  overlay.classList.remove('modal__overlay--close');
+});
+
+close.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modal.classList.add('modal-close');
+  modal.classList.remove('modal-open');
+  overlay.classList.add('modal__overlay--close');
+  overlay.classList.remove('modal__overlay--open');
 });
